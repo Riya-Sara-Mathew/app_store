d748c11e3fe329e093b01906c06cb5f523121509
krittysvarghese11 <krittysvarghese11@gmail.com>
2020-12-12 15:38:27 +0530

    Add Welcome Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:app_store/constants.dart';
 import 'package:app_store/screens/bars.dart';
+import 'package:app_store/widgets/rounded_button.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,7 +20,79 @@ class MyApp extends StatelessWidget {
           bodyText2: TextStyle(color: ksecondaryColor),
         ),
       ),
-      home: HomeScreen(),
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/Bitmap.png"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.display3,
+                children: [
+                  TextSpan(
+                    text: "logo",
+                  ),
+                  TextSpan(
+                    text: "phile",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .5,
+              child: RoundedButton(
+                text: "Sign Up",
+                fontSize: 17,
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return HomeScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .5,
+              child: RoundedButton(
+                text: "Sign In",
+                fontSize: 17,
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return HomeScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
